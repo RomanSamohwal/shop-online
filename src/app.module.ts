d7d0557b098e7c26b12974cb09386c5f3bb76aca
Roman <rsamohwal@gmail.com>
2022-12-30 13:23:10 +0300

    Добавил brand.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TypeModule } from './type/type.module';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { BrandModule } from './brand/brand.module';
 
 @Module({
-  imports: [TypeModule]
+  imports: [TypeModule,
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'online-shop',
+      models: [],
+      autoLoadModels: true,
+    }),
+    BrandModule,
+  ],
 })
-export class AppModule {}
+export class AppModule {
+}
